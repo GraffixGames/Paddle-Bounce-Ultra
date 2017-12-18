37cb45ca8a2499d34ad6bb977064a745a35830c5
BishopGaunt <anthonynlarosa@gmai.com>
2017-12-18 09:40:49 -0500

    circle

--- Paddle Bounce Ultra/GameScene.swift
+++ Paddle Bounce Ultra/GameScene.swift
@@ -11,16 +11,22 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var playerCore = SKShapeNode()
+    
     override func didMove(to view: SKView) {
-        
+        createPlayerCore()
     }
     
-    func oneLittleCircle(){
-        var Circle = SKShapeNode(circleOfRadius: 100 )
-        Circle.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
-        Circle.strokeColor = SKColor.black
-        Circle.glowWidth = 1.0
-        Circle.fillColor = SKColor.orange
-        self.addChild(Circle)
+    func createPlayerCore() {
+        let playerCore = SKShapeNode(circleOfRadius: 100 )
+        playerCore.name = "playerCore"
+        playerCore.position = CGPoint(x: frame.width/2, y: -frame.height/2)
+        playerCore.fillColor = UIColor.blue
+        playerCore.physicsBody = SKPhysicsBody(circleOfRadius: 100)
+        playerCore.physicsBody?.affectedByGravity = false
+        playerCore.physicsBody?.friction = 0
+        playerCore.physicsBody?.isDynamic = false
+        self.addChild(playerCore)
     }
+    
 }
