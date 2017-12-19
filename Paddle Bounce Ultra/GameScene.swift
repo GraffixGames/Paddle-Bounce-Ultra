4fd0990f9ac940a94a940c34b3e224ac661c0788
BishopGaunt <anthonynlarosa@gmai.com>
2017-12-19 10:40:13 -0500

    bit Mask

--- Paddle Bounce Ultra/GameScene.swift
+++ Paddle Bounce Ultra/GameScene.swift
@@ -9,18 +9,39 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory {
+	static let none: UInt32 = 0
+	static let playerCore: UInt32 = 0b1
+	
+	static let goodBall: UInt32 = 0b100
+	static let badBall: UInt32 = 0b1000
+}
+
 class GameScene: SKScene {
     
     var playerCore = SKShapeNode()
     var projectile = SKShapeNode()
-    
+	var goodBall = SKShapeNode()
+	var badBall = SKShapeNode()
+
     override func didMove(to view: SKView) {
+		physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+		playerCore = childNode(withName: "playerCore") as! SKShapeNode
+		goodBall = childNode(withName: "goodBall") as! SKShapeNode
+		badBall = childNode(withName: "badBall") as! SKShapeNode
+		playerCore.physicsBody?.categoryBitMask = PhysicsCategory.playerCore
+		goodBall.physicsBody?.categoryBitMask = PhysicsCategory.goodBall
+		badBall.physicsBody?.categoryBitMask = PhysicsCategory.badBall
         createPlayerCore()
         let spawnBall = SKAction.run {
             self.createProjectile()
         }
         run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 3), spawnBall])))
     }
+	
+//	func didBegin(_ contact: SKPhysicsContact) {
+//		
+//	}
     
     func createPlayerCore() {
         playerCore = SKShapeNode(circleOfRadius: 75 )
@@ -47,7 +68,13 @@ class GameScene: SKScene {
         let height = Double(arc4random_uniform(UInt32(frame.height - radius * 2))) + Double(radius)
         projectile.position = CGPoint(x: width, y: -height)
 		projectile.physicsBody = SKPhysicsBody(circleOfRadius: radius)
-        projectile.physicsBody?.isDynamic = false
+        projectile.physicsBody?.isDynamic = true
+		projectile.physicsBody?.allowsRotation = false
+		projectile.physicsBody?.affectedByGravity = false
+		projectile.physicsBody?.friction = 0
+		projectile.physicsBody?.restitution = 1
+		projectile.physicsBody?.linearDamping = 0
+		projectile.physicsBody?.velocity = CGVector(dx: 300, dy: -300)
         self.addChild(projectile)
     }
 
