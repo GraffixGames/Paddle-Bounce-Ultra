1955110b63da1b35a790f57fcc1166a0fa147835
Noah Graff <graffixgames@gmai.com>
2017-12-22 09:40:53 -0500

    some stuff idk

--- Paddle Bounce Ultra/GameScene.swift
+++ Paddle Bounce Ultra/GameScene.swift
@@ -12,21 +12,21 @@ import GameplayKit
 struct PhysicsCategory {
     static let none: UInt32 = 0
     static let playerCore: UInt32 = 0b1
-    
+	static let playerPaddle: UInt32 = 0b10
     static let goodBall: UInt32 = 0b100
     static let badBall: UInt32 = 0b1000
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-	var playerCore = SKShapeNode()
-	var playerPaddle = SKShapeNode()
-	var projectile = SKShapeNode()
-	var goodBalls = [SKShapeNode]()
-	var badBalls = [SKShapeNode]()
-	
-	let PLAYER_SPEED: CGFloat = 8
-	
+    var playerCore = SKShapeNode()
+    var playerPaddle = SKShapeNode()
+    var projectile = SKShapeNode()
+    var goodBalls = [SKShapeNode]()
+    var badBalls = [SKShapeNode]()
+    
+    let PLAYER_SPEED: CGFloat = 8
+    
     let moveAnalogStick = AnalogJoystick(diameter: 110)
     let rotateAnalogStick = AnalogJoystick(diameter: 110)
     
@@ -53,58 +53,65 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         moveAnalogStick.stick.radius = 45
         rotateAnalogStick.substrate.radius = 75
         rotateAnalogStick.stick.radius = 45
-		
+        moveAnalogStick.substrate.alpha = 0.5
+        moveAnalogStick.stick.alpha = 0.5
+        rotateAnalogStick.substrate.alpha = 0.5
+        rotateAnalogStick.stick.alpha = 0.5
+        
+        
+        
+        
         moveAnalogStick.trackingHandler = { [unowned self] data in
-			self.playerCore.physicsBody?.velocity = CGVector(dx: data.velocity.x * self.PLAYER_SPEED, dy: data.velocity.y * self.PLAYER_SPEED)
+            self.playerCore.physicsBody?.velocity = CGVector(dx: data.velocity.x * self.PLAYER_SPEED, dy: data.velocity.y * self.PLAYER_SPEED)
         }
-		
-		moveAnalogStick.stopHandler = { [unowned self] in
-			self.playerCore.physicsBody?.velocity = CGVector()
-		}
-		
+        
+        moveAnalogStick.stopHandler = { [unowned self] in
+            self.playerCore.physicsBody?.velocity = CGVector()
+        }
+        
         rotateAnalogStick.trackingHandler = { [unowned self] jData in
             if abs(jData.velocity.x) > 4 || abs(jData.velocity.y) > 4 {
                 self.playerPaddle.zRotation = jData.angular + CGFloat.pi / 2
             }
         }
-		
-		let spawnBall = SKAction.run {
-			self.createProjectile()
-		}
-		run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 3), spawnBall])))
-		playerCore = childNode(withName: "playerCore") as! SKShapeNode
-		playerCore.physicsBody?.categoryBitMask = PhysicsCategory.playerCore
-		physicsWorld.contactDelegate = self
-		playerCore.physicsBody!.contactTestBitMask = PhysicsCategory.goodBall | PhysicsCategory.badBall
+        
+        let spawnBall = SKAction.run {
+            self.createProjectile()
+        }
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 3), spawnBall])))
+        playerCore = childNode(withName: "playerCore") as! SKShapeNode
+        playerCore.physicsBody?.categoryBitMask = PhysicsCategory.playerCore
+        physicsWorld.contactDelegate = self
+        playerCore.physicsBody!.contactTestBitMask = PhysicsCategory.goodBall | PhysicsCategory.badBall
     }
-	
-	func didBegin(_ contact: SKPhysicsContact) {
-		if (contact.bodyA.categoryBitMask == PhysicsCategory.playerCore) || (contact.bodyB.categoryBitMask == PhysicsCategory.playerCore) {
-			if (contact.bodyA.categoryBitMask == PhysicsCategory.goodBall) || (contact.bodyB.categoryBitMask == PhysicsCategory.goodBall) {
-				print("goodBall")
-				if contact.bodyA.categoryBitMask == PhysicsCategory.goodBall {
-					contact.bodyA.node?.removeFromParent()
-				}
-				else {
-					contact.bodyB.node?.removeFromParent()
-				}
-			}
-			else if (contact.bodyA.categoryBitMask == PhysicsCategory.badBall) || (contact.bodyB.categoryBitMask == PhysicsCategory.badBall) {
-				print("badBall")
-				if contact.bodyA.categoryBitMask == PhysicsCategory.badBall {
-					contact.bodyA.node?.removeFromParent()
-				}
-				else {
-					contact.bodyB.node?.removeFromParent()
-				}
-			}
-		}
-	}
-	
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == PhysicsCategory.playerCore) || (contact.bodyB.categoryBitMask == PhysicsCategory.playerCore) {
+            if (contact.bodyA.categoryBitMask == PhysicsCategory.goodBall) || (contact.bodyB.categoryBitMask == PhysicsCategory.goodBall) {
+                print("goodBall")
+                if contact.bodyA.categoryBitMask == PhysicsCategory.goodBall {
+                    contact.bodyA.node?.removeFromParent()
+                }
+                else {
+                    contact.bodyB.node?.removeFromParent()
+                }
+            }
+            else if (contact.bodyA.categoryBitMask == PhysicsCategory.badBall) || (contact.bodyB.categoryBitMask == PhysicsCategory.badBall) {
+                print("badBall")
+                if contact.bodyA.categoryBitMask == PhysicsCategory.badBall {
+                    contact.bodyA.node?.removeFromParent()
+                }
+                else {
+                    contact.bodyB.node?.removeFromParent()
+                }
+            }
+        }
+    }
+    
     
     override func didFinishUpdate() {
         playerPaddle.position.x = playerCore.position.x + lengthDir(length: 120, dir: playerPaddle.zRotation).x
-		playerPaddle.position.y = playerCore.position.y + lengthDir(length: 120, dir: playerPaddle.zRotation).y
+        playerPaddle.position.y = playerCore.position.y + lengthDir(length: 120, dir: playerPaddle.zRotation).y
     }
     
     func createPlayerCore() {
@@ -114,7 +121,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         playerCore.position = CGPoint(x: frame.midX, y: frame.midY)
         playerCore.fillColor = UIColor.blue
         playerCore.physicsBody = SKPhysicsBody(circleOfRadius: size)
-		playerCore.physicsBody?.isDynamic = true
+        playerCore.physicsBody?.isDynamic = true
         playerCore.physicsBody?.affectedByGravity = false
         playerCore.physicsBody?.allowsRotation = false
         self.addChild(playerCore)
